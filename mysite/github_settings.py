2f741e24ef093e0a7340094f177df82ab78cc48e
haden96 <charkiewiczdaniel@gmail.com>
2020-08-11 17:08:54 +0200

    Complete course

---
+++ mysite/github_settings.py
@@ -0,0 +1,16 @@
+# Using PythonAnywhere here are some settings:
+
+# Application name: 
+# Homepage Url: https://drchuck.pythonanywhere.com
+# Application Description: Whatever
+# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
+
+# Also on PythonAnywhere, go into the Web tab and enable "Force HTTPS"
+# so you don't get a redirect URI mismatch.
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = 'dbd5da50c8e5425c398e'
+SOCIAL_AUTH_GITHUB_SECRET = '423df3f1a592ef99c0d96927ce15df69d9c9d859'
+
+# For detail: https://readthedocs.org/projects/python-social-auth/downloads/pdf/latest/
